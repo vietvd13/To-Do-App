694813e36db8647496b109e41b865e17a9ee5ea2
vietvd13 <vuducviet0131@gmail.com>
2021-04-21 13:47:15 +0700

    [ToDoApp] Update style ToDoInput

--- src/components/ToDoInput.vue
+++ src/components/ToDoInput.vue
@@ -1,13 +1,15 @@
 <template>
   <div>
-    <input 
-      type="text" 
-      name="Input New Task" 
-      class="todo--input" 
-      spellcheck="false"
-      v-model="newTask"
-      @keyup.enter="addNewTask()"
-    >
+    <div class="zone-input">
+      <input 
+        type="text" 
+        name="Input New Task" 
+        class="todo--input" 
+        spellcheck="false"
+        v-model="newTask"
+        @keyup.enter="addNewTask()"
+      >
+    </div>
   </div>
 </template>
 
@@ -38,11 +40,25 @@ export default {
 </script>
 
 <style scoped>
- .todo--input {
+  .zone-input {
     width: 100%;
-    height: 40px;
-    padding: 5px 10px 5px 10px;
-    font-size: 25px;
+    height: 100%;
+    overflow: hidden;
+    text-align: center;
+    padding: 7px 15px 7px 15px;
+    background-color: #007580;
+    border-radius: 5px;
+  }
+
+ .todo--input {
+    width: 90%;
+    height: 25px;
+    padding: 5px 5px 5px 5px;
+    font-size: 18px;
+    overflow: hidden;
+    margin: 0 auto;
+    border-radius: 5px;
+    border: none;
  }
 
  .todo--input:focus {
